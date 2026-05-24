a627b1f8299c4eea2f5cbe658f0cc5f2655362e0
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-24 09:44:26 +0530

    C++

--- lect03(basic).cpp
+++ lect03(basic).cpp
@@ -5,3 +5,13 @@ int main()
     cout<<"Hello World!";
     return 0;
 }
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<2+3;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World!";
+    cout<<2+3;
     return 0;
 }
