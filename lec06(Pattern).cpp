b4b9ef2b3215eb3109c8b1659a2f7315ab32ee70
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-25 09:10:03 +0530

    C++

--- lec06(Pattern).cpp
+++ lec06(Pattern).cpp
@@ -11,10 +11,25 @@ int main()
 }
 
 
+
 /*To Print 
-           *****
-           *****
-           *****
-           *****
-           *****
+           * * * * *
+           * * * * *
+           * * * * *
+           * * * * *
+           * * * * *
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int j=1;j<=5;j++)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,21 @@
+/*To Print 
+           * * * * *
+           * * * * *
+           * * * * *
+           * * * * *
+           * * * * *
+*/
 #include<iostream>
 using namespace std;
 int main()
 {
-    for(int i=1;i<=5;i++)
+    for(int j=1;j<=5;j++)
     {
-        cout<<"*";
+        for(int i=1;i<=5;i++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
