3c8d70b37dcf75690462b8d8499043bd649d16f6
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-25 08:09:01 +0530

    C++

--- lec05(adv_loops).cpp
+++ lec05(adv_loops).cpp
@@ -118,14 +118,23 @@ using namespace std;
 int main()
 {
     int num;
-    cout<<"Enter a number to check if the number is Prime or not : ";
+    cout<<"Enter a number to check wheather it is Prime or not : ";
     cin>>num;
-    for(int i=2;i<num;i++)
+    if(num<2)
     {
-        if(num%i==0)
-        cout<<"Number is not prime number";
-        else
-        cout<<"Number is Prime Number";
+        cout<<"Not Prime";
+        return 0;
     }
-    return 0;
+    else
+    {
+        for(int i=2;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                cout<<"Not Prime";
+                return 0;
+            }
+        }
+        cout<<"Prime";
+    }
 }
