93ce84dcaf4d1e290a232df4944496b46463866f
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-25 09:46:35 +0530

    C++

--- lec06(Pattern).cpp
+++ lec06(Pattern).cpp
@@ -152,3 +152,19 @@ int main()
     }
     return 0;
 }
+
+
+
+/*To Print
+            a a a a a
+            b b b b b
+            c c c c c
+            d d d d d
+            e e e e e
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+}
