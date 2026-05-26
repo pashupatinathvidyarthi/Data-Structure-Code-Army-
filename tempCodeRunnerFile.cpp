d04cc6c310a5a8bd34f8a37ead1ad73dc8501eaf
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 19:28:39 +0530

    C++

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 int main()
 {
-    for(int j=1;j<=5;j++)
+    int i,j;
+    for(j=1;j<=5;j++)
     {
-        for(int i=1;i<=5-j;i++)
+        for(i=1;i<=5-j;i++)
         {
             cout<<"  ";
         }
-        for(int i=1;i<=2*j-1;i++)
+        for(i=1;i<=j;i++)
         {
-            cout<<"* ";
+            cout<<i<<" ";
+        }
+        for(i=j-1;i>=1;i--)
+        {
+            cout<<i<<" ";
         }
         cout<<endl;
     }
