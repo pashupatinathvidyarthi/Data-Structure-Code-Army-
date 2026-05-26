afd46a9eef15bf0c2476bce6bb6183201f7de491
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 15:37:20 +0530

    C++

--- lec07(Pattern-02).cpp
+++ lec07(Pattern-02).cpp
@@ -196,18 +196,3 @@ int main()
             5 4 3 2
             5 4 3 2 1
 */
-#include<iostream>
-using namespace std;
-int main()
-{
-    int count=5;
-    for(int j=5;j>=1;j--)
-    {
-        for(int i=j;i>=1;i--)
-        {
-            cout<<i<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
