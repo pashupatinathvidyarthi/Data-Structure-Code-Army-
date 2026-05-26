ecb659de1e95d343c7e5305ea03850ebb1fa713d
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 16:19:22 +0530

    C++

--- lec08(HARD_Pattern).cpp
+++ lec08(HARD_Pattern).cpp
@@ -81,3 +81,20 @@ int main()
     }
     return 0;
 }
+
+
+
+
+/*To Print
+                        a
+                      a b
+                    a b c
+                  a b c d
+                a b c d e
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+}
