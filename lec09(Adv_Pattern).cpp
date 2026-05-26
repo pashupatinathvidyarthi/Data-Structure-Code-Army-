9a150a3aac4f1250abd90802644a8e1ca051ce27
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 19:07:50 +0530

    C++

--- lec09(Adv_Pattern).cpp
+++ lec09(Adv_Pattern).cpp
@@ -11,6 +11,17 @@
 using namespace std;
 int main()
 {
-    
+    for(int j=1;j<=5;j++)
+    {
+        for(int i=1;i<=5-j;i++)
+        {
+            cout<<"  ";
+        }
+        for(int i=1;i<=2*j-1;i++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,24 +1,18 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
-    for(int j=1; j<=5; j++)
+    for(int j=1;j<=5;j++)
     {
-        // Space printing
-        for(int i=1; i<=5-j; i++)
+        for(int i=1;i<=5-j;i++)
         {
             cout<<"  ";
         }
-
-        // Number printing
-        for(int col=j; col>=1; col--)
+        for(int i=1;i<=2*j-1;i++)
         {
-            cout<<col<<" ";
+            cout<<"* ";
         }
-
         cout<<endl;
     }
-
     return 0;
 }
