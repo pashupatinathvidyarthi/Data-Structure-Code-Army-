8a677fcbde8a3eeef9a484a73f3e9e0645a43267
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 16:40:02 +0530

    C++

--- lec08(HARD_Pattern).cpp
+++ lec08(HARD_Pattern).cpp
@@ -146,8 +146,20 @@ int main()
 */
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    
+    for(int j=1; j<=5; j++)
+    {
+        for(int i=1; i<=5-j; i++)
+        {
+            cout<<"  ";
+        }
+        for(int col=j; col>=1; col--)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,24 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    for(int j=1;j<=5;j++)
+    for(int j=1; j<=5; j++)
     {
-        for(int i=1;i<=5-j;i++)
+        // Space printing
+        for(int i=1; i<=5-j; i++)
         {
             cout<<"  ";
         }
-        for(int col=1;col<=j;col++)
+
+        // Number printing
+        for(int col=j; col>=1; col--)
         {
-            char name='A'+col-1;
-            cout<<name<<" ";
+            cout<<col<<" ";
         }
+
         cout<<endl;
     }
+
     return 0;
 }
