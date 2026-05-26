e9c3bc10c68794130dd09e7ca275c68ad19699f8
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-26 15:52:44 +0530

    C++

--- lec08(HARD_Pattern).cpp
+++ lec08(HARD_Pattern).cpp
@@ -11,10 +11,12 @@ int main()
 {
     for(int j=1;j<=5;j++)
     {
+        //To print space
         for(int i=1;i<=5-j;i++)
         {
             cout<<" ";
         }
+        //To print *
         for(int col=1;col<=j;col++)
         {
             cout<<"*";
