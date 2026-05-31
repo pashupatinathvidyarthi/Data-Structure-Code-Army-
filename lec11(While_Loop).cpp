513ae495074bfee2f06f1eeb2cb7bf3c67e34736
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-05-31 09:04:08 +0530

    C++

--- lec11(While_Loop).cpp
+++ lec11(While_Loop).cpp
@@ -14,3 +14,16 @@ int main()
 
 
 //To print table of 8
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=8;
+    int j=1;
+    while(j<=10)
+    {
+        cout<<i<<" * "<<j<<" = "<<i*j<<endl;
+        j++;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,14 @@
-//To print number from 1 to 10
+//To print table of 8
 #include<iostream>
 using namespace std;
 int main()
 {
-    int i=1;
-    while(i<=10)
+    int i=8;
+    int j=1;
+    while(j<=10)
     {
-        cout<<i<<" ";
-        i++;
+        cout<<i<<" * "<<j<<" = "<<i*j<<endl;
+        j++;
     }
     return 0;
 }
