ea16319af3d4003e21a68e299ef36d63bbcea2a6
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-01 07:59:54 +0530

    C++

--- lec11(While_Loop).cpp
+++ lec11(While_Loop).cpp
@@ -124,3 +124,26 @@ int main()
 
 
 //Switch
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"Enter a number : ";
+    cin>>i;
+
+    switch(i)
+    {
+        case 1:
+        cout<<"Rohit";
+        break;
+
+        case 2:
+        cout<<"Mohit";
+        break;
+
+        default:
+        cout<<"Sohit";
+    }
+    return 0;
+}
