c4c792bea71a4dc8b1c40f6ff19efbed8c9c6f19
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-01 17:26:35 +0530

    C++

--- lec14(function).cpp
+++ lec14(function).cpp
@@ -1,2 +1,13 @@
-#include<iostream>
-using namespace std;
+/*
+---------------------------------------
+            Function Syntax
+---------------------------------------
+return_type function_name(parameter1, parameter2, para.....)
+{
+    {
+        code part
+    }
+    return value;
+}
+
+*/
