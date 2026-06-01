86853d2d754d87dc5f144f3e5cb4d89af161d072
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-01 14:18:05 +0530

    C++

--- lec12(dec_bin,dec_oct,bin_oct).cpp
+++ lec12(dec_bin,dec_oct,bin_oct).cpp
@@ -20,3 +20,28 @@ int main()
     cout<<"Binary of "<<number<<" is : "<<ans;
     return 0;
 }
+
+
+
+//To convert decimal to binary (using Bitwise Operator)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a decimal number to get it's binary : ";
+    cin>>num;
+    int number=num;
+    int rem;
+    int ans=0;
+    int mul=1;
+    while(num>0)
+    {
+        rem=num&1;
+        num=num>>1;
+        ans=rem*mul+ans;
+        mul=mul*10;
+    }
+    cout<<"Binary of "<<number<<" is : "<<ans;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -12,11 +12,11 @@ int main()
     int mul=1;
     while(num>0)
     {
-        rem=num%2;
-        num=num/2;
+        rem=num&1;
+        num=num>>1;
         ans=rem*mul+ans;
         mul=mul*10;
     }
-    cout<<"Binary of"<<number<<" is : "<<ans;
+    cout<<"Binary of "<<number<<" is : "<<ans;
     return 0;
 }
