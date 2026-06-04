6b4e8a7e83b577df7b3fd53e394cf5b6a9180cf9
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-04 07:31:26 +0530

    C++

--- lec16(Arrays).cpp
+++ lec16(Arrays).cpp
@@ -1,2 +1,13 @@
 //Array is a linear collection of similar elements.
 //Array is also known as subscript variable.  subscript(x1,x2)
+
+//Write a program to calculate the average of 100 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[100];
+    int sum=0;
+    float avg;
+    
+}
