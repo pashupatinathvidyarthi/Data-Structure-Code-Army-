3aac8c1b0c3df0745fdadf296291975f1da73e01
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-05 06:43:07 +0530

    C++

--- lec16_Arrays.cpp
+++ lec16_Arrays.cpp
@@ -50,3 +50,21 @@ int a[5]={1,2,3,4,5}; // we can initialize array during declaration
 
 
 //Function Call by Passing an Array
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10];
+    funarr(a,10);
+    return 0;
+}
+
+void funarr(int b[],int n)
+{
+    int i;
+    cout<<"Enter "<<n<<"numbers : ";
+    for(i=0;i<n;i++)
+    {
+        cin>>b[i];
+    }
+}
