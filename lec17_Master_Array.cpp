73e902d8853d9b7a024a09878e0e2e4acec5a8fc
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-06 08:22:31 +0530

    C++

--- lec17_Master_Array.cpp
+++ lec17_Master_Array.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 int main()
 {
-    
+    int arr[5]={56,78,32,45,67};
+    int num;
+    cout<<"Enter a elemrnt to be search in an array : ";
+    cin>>num;
+    for(int i=0;i<arr[i];i++)
+    {
+        
+    }
 }
