5938b20d357f496dcdb73b5db0834fb94e1c34d4
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-06 08:26:25 +0530

    C++

--- lec17_Master_Array.cpp
+++ lec17_Master_Array.cpp
@@ -5,11 +5,16 @@ using namespace std;
 int main()
 {
     int arr[5]={56,78,32,45,67};
-    int num;
+    int num,a;
     cout<<"Enter a elemrnt to be search in an array : ";
     cin>>num;
     for(int i=0;i<arr[i];i++)
     {
-        
+        if(num==arr[i])
+        {
+            cout<<"Yes Present";
+            return 1;
+        }
     }
+
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,17 @@
-//Find minimum value in an Array
 #include<iostream>
 using namespace std;
 int main()
 {
-    int arr[5]={5,674,34,34,2};
-    int ans= INT_MAX;
-    for(int i=0;i<5;i++)
+    int arr[5]={56,78,32,45,67};
+    int num,a;
+    cout<<"Enter a elemrnt to be search in an array : ";
+    cin>>num;
+    for(int i=0;i<arr[i];i++)
     {
-        if(ans>arr[i])
-        ans=arr[i];
+        if(num==arr[i])
+        {
+            cout<<"Yes Present";
+            return 1;
+        }
     }
-    cout<<ans;
-    return 0;
 }
