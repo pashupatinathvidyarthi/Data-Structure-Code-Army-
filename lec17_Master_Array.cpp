1d30303c1e2310aff80db89cc61612db53b1dbc8
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-06 09:03:40 +0530

    C++

--- lec17_Master_Array.cpp
+++ lec17_Master_Array.cpp
@@ -26,5 +26,22 @@ int main()
 using namespace std;
 int main()
 {
-    
+    int arr1[5]={4,5,6,7,5};
+    int arr2[5];
+    int j=0;
+    for(int i=4;i>=0;i--)
+    {
+        arr2[j]=arr1[i];
+        j++;
+    }
+    for(int i=0;i<5;i++)
+    {
+        arr1[i]=arr2[j];
+        j++;
+    }
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr1[i]<<" ";
+    }  
+    return 0; 
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 int main()
 {
-    int arr[5]={56,78,32,45,67};
-    int num,a;
-    cout<<"Enter a elemrnt to be search in an array : ";
-    cin>>num;
-    for(int i=0;i<arr[i];i++)
+    int arr1[5]={4,5,6,7,5};
+    int arr2[5];
+    int j=0;
+    for(int i=4;i>=0;i--)
     {
-        if(num==arr[i])
-        {
-            cout<<"Yes Present";
-            return 1;
-        }
+        arr2[j]=arr1[i];
+        j++;
     }
+    for(int i=0;i<5;i++)
+    {
+        arr1[i]=arr2[j];
+        j++;
+    }
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr1[i];
+    }  
+    return 0; 
 }
