b852d38b1e7cf3e237a93fb7a938c670ee04b649
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-06-17 10:08:43 +0530

    C++

--- lec18_(Time_Complexity).cpp
+++ lec18_(Time_Complexity).cpp
@@ -5,3 +5,17 @@
 It is the total time taken by an algorithm to run as a function of 
 length of the input
 */
+
+
+
+//Find time complexity of below programs
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    for(i=1; i<=n; i++)
+    cout<<"Chamka";
+    return 0;
+}
